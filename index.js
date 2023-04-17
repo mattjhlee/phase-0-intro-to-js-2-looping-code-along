32a24d31b4d78f0ffed032189de129ca4d250601
Matthew <mattjhlee25@gmail.com>
2023-04-17 03:01:52 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, string) {
+    let message = [];
+    for (let i = 0; i < name.length; i++) {;
+        message.push(`Thank you, ${name[i]}, for the wonderful ${string} gift!`);
+    }
+    return message;
+}
+function countDown(n) {
+    let i = n;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
